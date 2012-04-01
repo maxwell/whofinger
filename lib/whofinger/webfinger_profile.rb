13a1cfd2eb1df5febd4823770d2c1695d980bb40
Maxwell Salzberg <maxwell@joindiaspora.com>
2012-04-01 17:02:12 -0400

    fix some bugs with the public_key method

--- lib/whofinger/webfinger_profile.rb
+++ lib/whofinger/webfinger_profile.rb
@@ -8,11 +8,11 @@ class WebfingerProfile
   end
 
   def public_key
-    content_at('public-key')
+    content_at('public-key').to_s.strip
   end
 
-  def content_at(rel)
-   if found_link = links.find{|x| x[rel] == rel}
+  def content_at(rel_value)
+   if found_link = links.find{|x| x['rel'] == rel_value}
       found_link['href']
    end
   end
